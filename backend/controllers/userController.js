962d5ccd6f22fb48a6b4abef8e97f50728d5d2db
unknown <unosega@gmail.com>
2022-11-28 00:10:31 +0700

    update folder frontend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -63,13 +63,7 @@ export const loginUser = asyncHandler(async (req, res) => {
 })
 
 export const getMe = asyncHandler(async (req, res) => {
-    const { _id, name, email } = await User.findById(req.user.id)
-
-    res.status(200).json({
-        id: _id,
-        name,
-        email
-    })
+    res.status(200).json(req.user)
 })
 
 // Generate JWT
